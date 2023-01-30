d051ae24723a72b3090730be6b9f3fc5f446e6b0
Jesús Pérez <jesus-perez95@hotmail.com>
2023-01-30 00:49:11 -0300

    asdasda

---
+++ Assets/Scripts/ObserverChannelArchitecture/Controllers/HighScoreController.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.ObserverChannelArchitecture.GUI;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.ObserverChannelArchitecture.Controllers
+{
+    public class HighScoreController : MonoBehaviour
+    {
+        [SerializeField] private VoidEventChannelSO _lostGameEvent;
+        [SerializeField] private GUIScoreIncreaser scoreSystem;
+        private int highscore = 0;
+        private Text text;
+
+
+        void Start()
+        {
+            text = GetComponent<Text>();
+            _lostGameEvent.OnEventRaised += CheckForHighScore;
+        }
+
+        void CheckForHighScore()
+        {
+            if (scoreSystem.Score > highscore)
+            {
+                highscore = scoreSystem.Score;
+                text.text = "High Score: " + highscore.ToString();
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ObserverChannelArchitecture/GUI/GUIComboDisplayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.ObserverChannelArchitecture.GUI
+{
+    public class GUIComboDisplayer : MonoBehaviour
+    {
+        [SerializeField] private VoidEventChannelSO _scoreIncreaseEvent;
+        private Text _text;
+
+        void Start()
+        {
+            _text = GetComponent<Text>();
+            _text.text = "";
+            _scoreIncreaseEvent.OnEventRaised += ComboUp;
+        }
+
+        private void ComboUp()
+        {
+            StartCoroutine(UpdatComboText());
+        }
+
+        private IEnumerator UpdatComboText()
+        {
+            _text.text = ("Good job!");
+            yield return new WaitForSeconds(1);
+            _text.text = "";
+        }
+    }
+}
